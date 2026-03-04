4db11d6b66345229005366b2f4c6f61377959861
Santyyy <santiagoeseiza10@gmail.com>
2026-03-04 15:53:07 -0300

    chore: añadido Test E2E

---
+++ tests/example.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "@playwright/test";
+
+// tests/auth.spec.ts
+test("usuario puede registrarse", async ({ page }) => {
+  await page.goto("/login");
+  await page.fill('[name="email"]', "test@test.com");
+  await page.fill('[name="password"]', "123456");
+  await page.click('button[type="submit"]');
+  await expect(page).toHaveURL("/dashboard");
+});
+
+test("usuario puede ver paquetes", async ({ page }) => {
+  await page.goto("/");
+  await expect(page.locator(".package-card")).toHaveCount(3);
+});
