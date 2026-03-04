3abd5accbf1998075fb103c224d4c90d9b99f420
Santyyy <santiagoeseiza10@gmail.com>
2026-03-04 17:07:49 -0300

    feat: CRUD de los paquetes añadida y funcional

--- lib/packages.ts
+++ lib/packages.ts
@@ -1,15 +1,20 @@
-function getPackages() {
-    
+import { db } from "./firebase";
+import { collection, getDocs, addDoc, updateDoc, deleteDoc, doc } from "firebase/firestore";
+
+export async function getPackages() {
+    const snap = await getDocs(collection(db, "packages"))
+    return snap.docs.map(d => ({id: d.id, ...d.data()}))
 }
 
-function createPackage(data) {
-
+export async function createPackage(data: object) {
+    return addDoc(collection(db, "packages"), data)
 }
 
-function updatePackage(id, data) {
-
+export async function updatePackage(id: string, data: object) {
+    return updateDoc(doc(db, "packages", id), data)
 }
 
-function deletePackage(id) {
+export async function deletePackage(id: string) {
+    return deleteDoc(doc(db, "packages", id))
 
 }
