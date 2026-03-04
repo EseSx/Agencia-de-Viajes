850ff4f6cc02fb204754a6c919ab539d9186d9c9
Santyyy <santiagoeseiza10@gmail.com>
2026-03-04 17:01:12 -0300

    feat: autenticación ahora tiene funcionalidad

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,14 +1,18 @@
-export signUp(email, password) {
-    
+import {auth} from "@/lib/firebase"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, signOut as firebaseSignOut } from "firebase/auth"
+
+export async function signUp(email: string, password: string) {
+    return createUserWithEmailAndPassword(auth, email, password)
 }
 
-export signIn(email, password) { 
+export async function signIn(email: string, password: string) { 
+    return signInWithEmailAndPassword(auth, email, password)
 }
 
-export signInWithGoogle() {
-    
+export async function signInWithGoogle() {
+    return signInWithPopup(auth, new GoogleAuthProvider())  
 }
 
-export signOut() {
-    
+export async function signOut() {
+    return firebaseSignOut(auth)
 }
