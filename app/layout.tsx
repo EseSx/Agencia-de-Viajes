e36378860c6bc660b80b10415c60a306533b2243
Santyyy <santiagoeseiza10@gmail.com>
2026-03-05 17:30:08 -0300

    feat: navbar añadido a la pagina principal

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import AuthSync from "@/components/AuthSync";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <AuthProvider>
           <AuthSync />
+          <Navbar />
           {children}
         </AuthProvider>
       </body>
