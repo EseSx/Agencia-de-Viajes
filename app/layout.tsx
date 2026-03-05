8cb3b4117ae5895338b5ce283594a1d3efcd4f77
Santyyy <santiagoeseiza10@gmail.com>
2026-03-05 16:45:12 -0300

    fix: "use client" retirado por no ser necesario

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-"use client"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         <AuthProvider>
           <AuthSync />
           {children}
